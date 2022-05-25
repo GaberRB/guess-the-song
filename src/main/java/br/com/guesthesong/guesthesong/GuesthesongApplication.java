629df19e0ee79e6678261ec4f6451774a61c6fa3
GaberRB <grios0311@gmail.com>
2022-05-25 19:54:54 -0300

    feat: Add proprities e novo projeto spring java 17

---
+++ src/main/java/br/com/guesthesong/guesthesong/GuesthesongApplication.java
@@ -0,0 +1,13 @@
+package br.com.guesthesong.guesthesong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuesthesongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuesthesongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guesthesong/guesthesong/GuesthesongApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guesthesong.guesthesong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuesthesongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
