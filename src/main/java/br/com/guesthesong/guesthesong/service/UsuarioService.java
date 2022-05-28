3cf8b1399dd28eca1c3c1cec7b526d5ae48fd559
GabrielFGCosta <bielfilipeg@gmail.com>
2022-05-27 23:51:43 -0300

    feat: Classe Usuario 2

---
+++ src/main/java/br/com/guesthesong/guesthesong/service/UsuarioService.java
@@ -0,0 +1,33 @@
+package br.com.guesthesong.guesthesong.service;
+
+import br.com.guesthesong.guesthesong.model.Usuario;
+import br.com.guesthesong.guesthesong.model.Usuario;
+import br.com.guesthesong.guesthesong.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario salvar(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    public List<Usuario> lista(){
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> buscarPorId(long id){
+        return usuarioRepository.findById(id);
+    }
+
+    public void remover(Long id){
+        usuarioRepository.deleteById(id);
+    }
+}
