bd1f7c4145104d74760912f6fea54c424cb5b952
GabrielFGCosta <bielfilipeg@gmail.com>
2022-05-31 01:05:13 -0300

    feat: Start criando usuario - Corrigido

--- src/main/java/br/com/guesthesong/guesthesong/model/Usuario.java
+++ src/main/java/br/com/guesthesong/guesthesong/model/Usuario.java
@@ -31,19 +31,5 @@ public class Usuario {
     @Column(name = "password", length = 12, nullable = false)
     private String password;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
 }
