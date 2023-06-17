2999a0a0788ceabe21bb519c055622c547a624dd
Gabriel Rios Belmiro <grios0311@gmail.com>
2023-06-17 18:08:25 -0300

    Update list-resources.py

--- .github/workflows/list-resources.py
+++ .github/workflows/list-resources.py
@@ -6,7 +6,8 @@ with open(template_path, 'r') as f:
     template = yaml.safe_load(f)
 
 resources = template.get('Resources', {})
-resource_types = [resource['Type'].split('::', 1)[-1].strip() for resource in resources.values()]
+resource_types = [resource['Type'].split('::')[-1].strip().split(':')[-1].strip() for resource in resources.values()]
 
 with open('resources.txt', 'w') as f:
     f.write('\n'.join(resource_types))
+
