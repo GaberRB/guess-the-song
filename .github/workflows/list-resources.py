421be1b9773b3024ce214aa795ef0180c7bace3e
Gabriel Rios Belmiro <grios0311@gmail.com>
2023-06-17 18:04:00 -0300

    Update list-resources.py

--- .github/workflows/list-resources.py
+++ .github/workflows/list-resources.py
@@ -6,7 +6,7 @@ with open(template_path, 'r') as f:
     template = yaml.safe_load(f)
 
 resources = template.get('Resources', {})
-resource_types = [resource['Type'].replace('AWS::', '') for resource in resources.values()]
+resource_types = [resource['Type'].split('::', 1)[-1].strip() for resource in resources.values()]
 
 with open('resources.txt', 'w') as f:
     f.write('\n'.join(resource_types))
