d33104d4ce294c005308af4ac6815c09805e2dc7
Gabriel Rios Belmiro <grios0311@gmail.com>
2023-06-17 17:15:10 -0300

    Update list-resources.js

--- list-resources.js
+++ list-resources.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
-const yaml = require('yaml');
+const YAML = require('yml');
 
 const templatePath = 'template.yml';
 const templateContents = fs.readFileSync(templatePath, 'utf8');
-const template = yaml.parse(templateContents);
+const template = YAML.parse(templateContents);
 
-const resources = Object.keys(template.Resources || {});
+const resources = Object.keys(template.Resources || []);
 
-console.log(resources.join('\n'));
-console.log("::set-output name=resources::" + JSON.stringify(resources));
-process.stdout.write(`::set-output name=resources::${JSON.stringify(resources)}`);
+fs.writeFileSync('resources.txt', resources.join('\n'));
