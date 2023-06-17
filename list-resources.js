09ed872d9240766b75628c9f0076f0e602fce600
Gabriel Rios Belmiro <grios0311@gmail.com>
2023-06-17 17:28:48 -0300

    Update list-resources.js

--- list-resources.js
+++ list-resources.js
@@ -5,6 +5,11 @@ const templatePath = 'template.yml';
 const templateContents = fs.readFileSync(templatePath, 'utf8');
 const template = yaml.parse(templateContents);
 
-const resources = Object.keys(template.Resources || []);
+const resources = Object.entries(template.Resources || {})
+  .map(([resourceName, resource]) => ({
+    name: resourceName,
+    type: resource.Type
+  }));
 
-fs.writeFileSync('resources.txt', resources.join('\n'));
+fs.writeFileSync('resources.txt', resources.map(resource => resource.type).join('\n'));
+
